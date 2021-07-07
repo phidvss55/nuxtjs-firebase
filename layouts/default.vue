ce364e95570a825478a0cf98f4be60105e8f228a
Phi Dinh <phi.dinh@whl.travel>
2021-07-07 16:51:40 +0700

    commit code

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <AppHeader />
+  </div>
+</template>
+
+<script>
+import AppHeader from '../components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader
+  }  
+}
+</script>
+
+<style>
+  
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <h2>Welcome to the best corny dad jokes app on the web</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About The App",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
